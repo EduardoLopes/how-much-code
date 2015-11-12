b3d436604deda113273325f33ebff61046a9d1dd
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-11-12 12:46:09 -0300

    Fix files changed count

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var readline = require('readline');
 var isArray = require('is-array');
 var NumstatParser  = require('./NumstatParser');
 
-var filesChanged = 0;
+var filesChanged = {};
 var insertions   = 0;
 var deletions    = 0;
 //for some reason the stream is getting one line without a commit
-var countCommits = -1;
+var countCommits = 0;
 
 
 var argsToInject = process.argv.slice(2);
@@ -66,7 +66,13 @@ spawn('git', args).stdout
     var data = new NumstatParser(parser);;
 
     for (var i = 0; i < data.files.length; i++) {
-      filesChanged += 1;
+
+      if(typeof filesChanged[data.files[i].file] == 'undefined'){
+        filesChanged[data.files[i].file] = 0;
+      }
+
+      filesChanged[data.files[i].file] += 1;
+
       insertions += data.files[i].insertions;
       deletions += data.files[i].deletions;
     };
@@ -85,9 +91,15 @@ spawn('git', args).stdout
 
   readline.clearLine(process.stdout, 0);
 
+  var filesChangedCount = 0;
+
+  for (var fileChanged in  filesChanged) {
+    filesChangedCount++;
+  }
+
   console.log('\n'+colors.cyan('----------------------------'));
   log( pluralSingulaFilter('Commit', 'Commits', countCommits) );
-  log( pluralSingulaFilter('File changed', 'Files changed', filesChanged) );
+  log( pluralSingulaFilter('File changed', 'Files changed', filesChangedCount) );
   log( pluralSingulaFilter('Insertion', 'Insertions', insertions) + colors.green('(+)') );
   log( pluralSingulaFilter('Deletion', 'Deletions', deletions) + colors.red('(-)') );
   console.log(colors.cyan('----------------------------'));
